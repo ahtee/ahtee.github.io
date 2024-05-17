4afc6b7cd24aea715d0060f11c71d0a063bac6d3
ahtee <5076085+ahtee@users.noreply.github.com>
2024-05-17 17:00:40 -0400

    Create Skills.js

---
+++ src/components/Skills.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Skills() {
+  return (
+    <div className="container skills">
+      <h2>Skills</h2>
+      <ul>
+        <li>JavaScript, React.js, HTML, CSS</li>
+        <li>AWS (EC2, S3, Lambda, RDS)</li>
+        <li>Terraform, Docker</li>
+        <li>Jenkins CI/CD, Infrastructure as Code</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Skills;
