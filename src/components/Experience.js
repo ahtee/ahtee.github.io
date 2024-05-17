9a9c6cde663d4b51f917c61ce1b42cba9ce5784b
ahtee <5076085+ahtee@users.noreply.github.com>
2024-05-17 17:01:33 -0400

    Create Experience.js

---
+++ src/components/Experience.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Experience() {
+  return (
+    <div className="container experience">
+      <h2>Experience</h2>
+      <h3>Site Reliability Engineer at General Electric</h3>
+      <p>2017 – Present</p>
+      <ul>
+        <li>Web application development and maintenance</li>
+        <li>Enhanced Terraform IaC, Jenkins CI/CD pipelines, secure Docker images</li>
+        <li>Cloud management portal enhancements</li>
+      </ul>
+      <h3>Associate Software Developer at Fidelity Investments</h3>
+      <p>2016 – 2017</p>
+      <ul>
+        <li>Developed features for internal Angular application</li>
+        <li>Implemented unit tests for code reliability</li>
+      </ul>
+      <h3>Software Engineering Specialist at Northern Kentucky University</h3>
+      <p>2013 – 2015</p>
+      <ul>
+        <li>Developed online web experiences using JavaScript and React.js</li>
+        <li>Collaborated with clients for customized web solutions</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Experience;
