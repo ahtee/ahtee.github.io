67da806087d2813ca54b6023c979809b48d8ed5b
ahtee <5076085+ahtee@users.noreply.github.com>
2024-05-17 17:01:04 -0400

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Benjamin Otte</h1>
+      <p>DevOps and Web Development Expert</p>
+    </header>
+  );
+}
+
+export default Header;
