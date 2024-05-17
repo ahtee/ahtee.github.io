7edd4e4043baba83f77d57b2847b8701b2398ad2
ahtee <5076085+ahtee@users.noreply.github.com>
2024-05-17 17:01:52 -0400

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 Benjamin Otte</p>
+      <p>Email: benottedev@gmail.com | GitHub: github.com/ahtee</p>
+    </footer>
+  );
+}
+
+export default Footer;
